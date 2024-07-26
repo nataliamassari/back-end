983262fe7a7e722c27fee43a455727fd26d5a887
Felipe Brás Coutinho De Oliveira <114227050+FelipeCoutinhoBras@users.noreply.github.com>
2024-07-26 19:49:28 -0300

    Add files via upload
    Implemento da sessão com usuário e senha salvos no arquivo dot env, e desenvolvimento de um css básico apenas para entender se a funcionalidade estava funcionando de fato.

--- app.js
+++ app.js
@@ -1,54 +1,59 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
 var cookieParser = require('cookie-parser');
-var session = require("express-session")
-var passport = require("passport"); 
-var LocalStrategy = require("passport-local").Strategy;
-
 var logger = require('morgan');
+var session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const dotenv = require('dotenv');
 
+// Inicialize a aplicação express
 var app = express();
 
-// view engine setup
-var mustacheExpress = require("mustache-express");
-var engine = mustacheExpress()
-app.engine("mustache", engine);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "mustache");
+dotenv.config();
+// Configure a sessão
+app.use(session({
+  secret: '$A1B2D4!A',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }  // Defina como 'true' se estiver usando HTTPS
+}));
+
+// Configure o mustache-express
+const mustacheExpress = require('mustache-express');
+const engine = mustacheExpress();
+app.engine('mustache', engine);
+
+// Configure o view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret:  "#@A4327Asdzw",
-  resave:  false,
-  saveUninitialized:  false
-}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Importar rotas
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// Usar rotas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-//precisa terminar de configurar passaport
-
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
