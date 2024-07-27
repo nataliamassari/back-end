e5f158be8d176d5a55155e61424e8fd8b58abd26
nataliamassari <naty.massari25@gmail.com>
2024-07-26 22:52:45 -0300

    libraries installed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+
 var cookieParser = require('cookie-parser');
+var session = require("express-session")
+var passport = require("passport"); 
+var LocalStrategy = require("passport-local").Strategy;
+
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -10,17 +15,27 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress()
+app.engine("mustache", engine);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret:  "#@A4327Asdzw",
+  resave:  false,
+  saveUninitialized:  false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//precisa terminar de configurar passaport
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
