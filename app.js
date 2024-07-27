4488af0ae4b55b6e4279ee3050103440554b24b2
nataliamassari <naty.massari25@gmail.com>
2024-07-26 22:36:31 -0300

    redo project base

--- app.js
+++ app.js
@@ -3,30 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
 
-const dotenv = require('dotenv');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Inicialize a aplicação express
 var app = express();
 
-dotenv.config();
-// Configure a sessão
-app.use(session({
-  secret: '$A1B2D4!A',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false }  // Defina como 'true' se estiver usando HTTPS
-}));
-
-// Configure o mustache-express
-const mustacheExpress = require('mustache-express');
-const engine = mustacheExpress();
-app.engine('mustache', engine);
-
-// Configure o view engine
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'mustache');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,26 +19,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Importar rotas
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-// Usar rotas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function(err, req, res, next) {
-  // Set locals, only providing error in development
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
