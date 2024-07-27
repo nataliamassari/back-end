aef71366fdf6d6e6a389e34a7dde25ebaacccb4c
nataliamassari <naty.massari25@gmail.com>
2024-07-27 19:39:27 -0300

    json object creation for page creation done

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
 var passport = require("passport");
+const fs = require('fs');
+const bodyParser = require('body-parser');
 var LocalStrategy = require("passport-local").Strategy;
 
 var logger = require("morgan");
@@ -20,6 +22,9 @@ var engine = mustacheExpress();
 app.engine("mustache", engine);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -37,7 +42,33 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-//precisa terminar de configurar passaport
+app.post('/add', (req, res) => {
+  const newItem = req.body;
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+          console.error('Erro ao ler o arquivo', err);
+          return res.status(500).send('Erro ao ler o arquivo');
+      }
+
+      let array = [];
+
+      if (data) {
+          array = JSON.parse(data);
+      }
+
+      array.push(newItem);
+
+      fs.writeFile('data.json', JSON.stringify(array, null, 2), (err) => {
+          if (err) {
+              console.error('Erro ao escrever no arquivo', err);
+              return res.status(500).send('Erro ao escrever no arquivo');
+          }
+
+          res.status(200).send('Item adicionado com sucesso');
+      });
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ public/javascripts/redirects.js
@@ -0,0 +1,16 @@
+
+export function redirectToLoginRoute() {
+    window.location.href = '/login'
+}
+
+export function redirectToCreateRoute() {
+    window.location.href = '/create'
+}
+
+export function redirectToManageRoute() {
+    window.location.href = '/pageManager'
+}
+
+export function redirectToMenuRoute() {
+    window.location.href = '/'
+}
