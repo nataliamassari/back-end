88e3e9c7467976b42c2734ef0875bd280154e2d2
FelipeCoutinhoBras <felipebrascoutinho@gmail.com>
2024-07-28 16:03:15 -0300

    Implemento das rotas /post e /logout juntamente de alterações no template de login, para que a sessão funcione.

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,53 @@
 var express = require("express");
 var router = express.Router();
+var lastRoute = 0;
+
+/*session authentication*/
+function isLogged (req, res, next) {
+  if (req.session && req.session.user) {
+    if (lastRoute == 0) {
+      return res.render("create")
+    } else {
+      return res.render("pageManager")
+    }
+  }
+  return res.redirect("/login");
+};
+
 
 /* pages. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "main" });
 });
 
+router.post('/login', function (req, res, next) {
+  let {usuario, senha} = req.body;
+
+  if(usuario === process.env.ADMIN_USER && senha === process.env.ADMIN_PASS) {
+    req.session.user = usuario;
+    res.render("pageManager");
+  } else {
+    res.render("login");
+  };
+});
+
 router.get("/login", function (req, res, next) {
   res.render("login", { title: "login" });
 });
 
-router.get("/create", function (req, res, next) {
+router.get('/logout', function (req, res, next) {
+  if (req.session.user) {
+    delete req.session.user
+  }
+  res.redirect('/')
+})
+
+router.get("/create", isLogged, function (req, res, next) {
+  lastRoute = 0;
   res.render("create", { title: "create" });
 });
 
-router.get("/pageManager", function (req, res, next) {
+router.get("/pageManager", isLogged, function (req, res, next) {
   res.render("pageManager", { title: "pageManager" });
 });
 
